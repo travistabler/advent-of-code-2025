af3a4544a7ca84d6a211309a72b1f3a8554156b3
travistabler <traviswtabler@gmail.com>
2025-12-02 15:17:32 -0600

    add some clarifying comments and format w/ black

--- day-two/day-two.py
+++ day-two/day-two.py
@@ -9,20 +9,24 @@ import pandas as pd
 script_dir = Path(__file__).parent
 input_file = script_dir / "input" / "day-two-input.csv"
 
-input = ['1-100','101-200', '200-10000']
+# input = ['1-100','101-200', '200-10000'] # for testing
 input_df = pd.read_csv(input_file, header=None)
 input_list = input_df.squeeze().tolist()
 input_list
 
 # %%
+# part one
 total = 0
 for item in input_list:
-    #print(item)
+    # print(item)
     id_range = item.split("-")
     for id in range(int(id_range[0]), int(id_range[1])):
         if len(str(id)) % 2 == 0:
-            first, second = str(id)[:len(str(id))//2 + len(str(id))%2], str(id)[len(str(id))//2 + len(str(id))%2:]
-            #print(first, second)
+            first, second = (
+                str(id)[: len(str(id)) // 2 + len(str(id)) % 2],
+                str(id)[len(str(id)) // 2 + len(str(id)) % 2 :],
+            )
+            # print(first, second)
             if first == second:
                 total += int(first + second)
 print(total)
@@ -32,7 +36,7 @@ print(total)
 # part two
 total = 0
 for item in input_list:
-    #print(item)
+    # print(item)
     id_range = item.split("-")
     for id in range(int(id_range[0]), int(id_range[1])):
         for repeat_len in range(1, len(str(id)) // 2 + 1):
@@ -41,4 +45,4 @@ for item in input_list:
                 if pattern * (len(str(id)) // repeat_len) == str(id):
                     total += id
                     break
-print(total) # yeah, don't put this inside the loops
+print(total)  # yeah, don't put this inside the loops
